afd2ea32371de2ba0ac9f82932a85a77d4c51237
Diers <Diers@LAPTOP-VTCD2RNQ>
2021-08-11 11:37:47 -0300

    Commita versão inicial do projeto com configs base do spring boot

---
+++ src/main/java/com/dacdoliveira/wsdlclientcountry/WsdlClientCountryApplication.java
@@ -0,0 +1,13 @@
+package com.dacdoliveira.wsdlclientcountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlClientCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlClientCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dacdoliveira/wsdlclientcountry/WsdlClientCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacdoliveira.wsdlclientcountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlClientCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
